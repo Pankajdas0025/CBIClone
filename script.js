f17b8aceebf4581a145150b6bca17c9a3c632ee3
MR.PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2024-11-04 20:45:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeOpen(){
+    let Change=document.getElementById("Lform");
+    Change.style.display="block";
+//    let Bblur=document.getElementById("Body");
+//    Bblur.style.opacity="0.1";
+}
+function changeClose(){
+    let Change=document.getElementById("Lform");
+    Change.style.display="none";
+//    let Bblur=document.getElementById("Body");
+//    Bblur.style.opacity="0.1";
+}
